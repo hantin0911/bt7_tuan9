95be5ddc97bebb811b3527fc488d5f8c30e7e6f5
Tien <duongdz12345789@gmail.com>
2026-05-13 00:42:49 +0700

    đổi tên biến / Violation

--- src/main/java/banksystem/Violation.java
+++ src/main/java/banksystem/Violation.java
@@ -4,13 +4,13 @@ package banksystem;
  * Lớp này đã được sửa lỗi Checkstyle để tuân thủ Google Style.
  */
 public class Violation {
-    private int x;
+    private int value;
 
     /**
      * Phương thức kiểm tra với định dạng chuẩn 2 spaces.
      */
     public void testMethod() {
         int shortName = 10;
-        System.out.println(x + shortName);
+        System.out.println(value + shortName);
     }
 }
